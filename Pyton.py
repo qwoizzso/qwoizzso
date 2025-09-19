6dce972a80e5307c5112ed84b5f06c4cf10d89b5
qwoizzso <vladsekalov@gmail.com>
2025-09-19 13:09:55 +0300

    Add files via upload

---
+++ Pyton.py
@@ -0,0 +1,12 @@
+Pyton 
+
+#создание списка и строки
+'abcdef', lst = list(txt)
+
+#организация стека
+stack =  
+stack.append('a')  
+stack.append('b')  
+stack.append('c')  
+print(stack)  
+# Вывод: [‘a’, ‘b’, ‘c’]
