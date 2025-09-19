aee3727a3c876d223798b3b1990e3498e8bb8dfb
qwoizzso <vladsekalov@gmail.com>
2025-09-19 13:10:07 +0300

    Add files via upload

---
+++ Java.java
@@ -0,0 +1,23 @@
+Java 
+//пример кода на java для записи переменной список
+import java.util.ArrayList;  
+class Main {  
+public static void main(String args) {  
+// Создаём пустой список  
+ArrayList<Integer> a = new ArrayList<Integer>();  
+// Добавляем элементы  
+a.add(1);  
+a.add(2);  
+a.add(3);  
+// Печатаем список  
+System.out.println(a);  
+}  
+}  
+
+//пример кода на java для организации стека
+
+• MyStack<Integer> stack = new MyStack<>(10);
+• stack.push(10);
+• stack.push(20);
+
+.java
