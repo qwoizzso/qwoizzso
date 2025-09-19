a582da5ba72aa9ab0b848e52f25a5e3c4db810c1
qwoizzso <vladsekalov@gmail.com>
2025-09-19 13:10:21 +0300

    Add files via upload

---
+++ C.cpp
@@ -0,0 +1,15 @@
+C++ 
+
+//Пример кода на c++ для задания переменной "список"
+#include <iostream>  
+#include <list>  
+int main() {  
+// Создание списка целых чисел  
+std::list<int> numbers = {1, 2, 3, 4, 5};  
+}  
+
+//пример кода на c++ для организации стека
+
+std::stack<std::string> stack
+
+.cpp
